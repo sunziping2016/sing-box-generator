6fad409621a13dbfed2220311fc74f6b0b9c6f52
Ziping Sun <me@szp.io>
2023-10-25 23:29:55 +0800

    feat(root): add project skeleton

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "A configuration generator for sing-box";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+      version = builtins.substring 0 8 lastModifiedDate;
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          sing-box-generator = pkgs.buildGoModule {
+            pname = "sing-box-generator";
+            inherit version;
+            src = ./.;
+            vendorSha256 = null;
+          };
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ go gopls gotools go-tools ];
+          };
+        });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.sing-box-generator);
+    };
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sunziping2016/sing-box-generator
+
+go 1.21
